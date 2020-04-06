b11b0a3effb5040b1841c5091133325e219af6d2
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2020-04-06 17:17:39 -0500

    MODIFICACIÓN: DETENER CUANDO a=0

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 def a_power_b(a, b):
     
-    repeticiones = b
+    repetir = b
     potencia = 1
 
-    for i in range(0,repeticiones):
+    for i in range(0,repetir):
         potencia = potencia * a
     
     return potencia
 
 
-
 a = int(input("Ingrese la base de la potencia a realizar: "))
-b = int(input("Ingrese el exponente al que desea elebar la base {}: ".format(a)))
 
-potencia = a_power_b(a, b)
+while a != 0:
+    b = int(input("Ingrese el exponente al que desea elebar la base {}: ".format(a)))
 
-print("El resultado de elevar {} a la {} es: {}".format(a,b,potencia))
+    potencia = a_power_b(a, b)
+
+    print("El resultado de elevar {} a la {} es: {}".format(a,b,potencia))
+
+    #Indicación para hacer que el programa se detenga:
+    print("SI YA NO DESEA CALCULAR MÁS POTENCIAS, CUANDO SE LE SOLICITE LA BASE, DIGITE 0")
+
+    a = int(input("Ingrese la base de la potencia a realizar: "))
+
+    if a ==0:
+        print("PROGRAMA FINALIZADO")
+
+    
+
+
+
 
 
 
