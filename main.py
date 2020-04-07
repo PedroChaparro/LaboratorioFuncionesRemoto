d7f46872f02876dd04ca1cad1a57297c6bd98754
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2020-04-06 19:06:12 -0500

    MODIFICACIÓN: AÑADIR CONTADORES Y TRY-EXCEPT

--- main.py
+++ main.py
@@ -8,25 +8,72 @@ def a_power_b(a, b):
     
     return potencia
 
+contador_errores = 0
+contador_pares = 0
+contador_impares = 0
 
-a = int(input("Ingrese la base de la potencia a realizar: "))
+#Este ciclo nos permite usar el try-except para intentar solucinar errores en la ejecución
+while True: 
+
+    #Si se introduce una base válida, el ciclo se rompe y se continúa con la línea 28
+    try: 
+        a = int(input("Ingrese un número entero que será la base de la potencia a realizar: "))
+        break
+
+    #Si no se introduce una base válida, se regresa el try.
+    except ValueError:
+        print("ERROR, ENTRADA NO VÁLIDA, INTENTELO DE NUEVO")
+        contador_errores = contador_errores + 1
+        
 
 while a != 0:
-    b = int(input("Ingrese el exponente al que desea elebar la base {}: ".format(a)))
 
-    potencia = a_power_b(a, b)
+    #Al igual que con la base, este ciclo permite solucionar los errores al solicitar el exponente
+    while True: 
+                
+        #Si el exponente es un entero, el código se ejecuta normal
+        try: 
 
-    print("El resultado de elevar {} a la {} es: {}".format(a,b,potencia))
+            b = int(input("Ingrese un número entero que será el exponente al que se elevará la base {}: ".format(a)))
 
-    #Indicación para hacer que el programa se detenga:
-    print("SI YA NO DESEA CALCULAR MÁS POTENCIAS, CUANDO SE LE SOLICITE LA BASE, DIGITE 0")
+            potencia = a_power_b(a, b)
 
-    a = int(input("Ingrese la base de la potencia a realizar: "))
+            if (potencia % 2 ==0):
+                contador_pares = contador_pares + 1
+            else:
+                contador_impares = contador_impares + 1
 
-    if a ==0:
-        print("PROGRAMA FINALIZADO")
+            print("El resultado de elevar {} a la {} es: {}".format(a,b,potencia))
 
-    
+            #Indicación para hacer que el programa se detenga:
+            print("SI YA NO DESEA CALCULAR MÁS POTENCIAS, CUANDO SE LE SOLICITE LA BASE, DIGITE 0")
+
+            #Se solicita una nueva base.
+            while True: 
+
+                try:
+                    a = int(input("Ingrese un número entero que será la base de la potencia a realizar: "))
+                    break
+                
+                except (ValueError):
+                    print("ERROR, ENTRADA NO VÁLIDA, INTENTE DE NUEVO")
+                    contador_errores = contador_errores + 1
+
+
+            #Si el usuario digita 0, el programa se detiene e imprime los contadores. 
+            if a ==0:
+                print("PROGRAMA FINALIZADO")
+                print("La cantidad de veces que la potencia fue par es: {}".format(contador_pares))
+                print("La cantidad de veces que la potencia fue impar es: {}".format(contador_impares))
+                print("La cantidad de errores que ocurrieron fue: {}".format(contador_errores))
+
+            break
+
+        #Si el exponente no es válido, se solicita nuevamente
+        except ValueError:
+            print("ERROR, ENTRADA NO VÁLIDA, INTENTE DE NUEVO")
+            contador_errores = contador_errores + 1
+
 
 
 
